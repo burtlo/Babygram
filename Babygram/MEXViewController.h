e3aaa23fcee7bc7b3e85edee3338b5847bb78e1e
Franklin Webber <franklin.webber@gmail.com>
2013-10-07 14:16:32 -0600

    Babygram with a working textview

--- Babygram/MEXViewController.h
+++ Babygram/MEXViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MEXViewController : UIViewController
+@interface MEXViewController : UIViewController <UIImagePickerControllerDelegate, UITextViewDelegate, UINavigationControllerDelegate>
 
 @end
--- Babygram/MEXViewController.m
+++ Babygram/MEXViewController.m
@@ -10,6 +10,10 @@
 
 @interface MEXViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *babyImage;
+@property (weak, nonatomic) IBOutlet UITextView *babyText;
+@property (weak, nonatomic) IBOutlet UIButton *doneButton;
+
 @end
 
 @implementation MEXViewController
@@ -18,6 +22,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [[self babyText] setDelegate:self];
+    [[self doneButton] setEnabled:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,63 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)takePicture:(id)sender {
+    NSLog(@"Pushing Button");
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    [picker setDelegate:self];
+    
+    [self presentViewController:picker animated:YES completion:^{
+        NSLog(@"Launching");
+    }];
+    
+}
+
+- (UIImage *)magicianImage {
+    return [UIImage imageNamed:@"magician.jpg"];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"Picked an Image");
+    
+    // TODO: make this based on the image selected
+
+//    UIImage *newBabyImage = [self magicianImage];
+//    [[self babyImage] setImage:newBabyImage];
+    
+    UIImage *newBabyImage = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+    [[self babyImage] setImage:newBabyImage];
+    
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    NSLog(@"Cancelled");
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+}
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
+    NSLog(@"shouldBeginEditing");
+    return YES;
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    [[self doneButton] setEnabled:YES];
+}
+
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView {
+    NSLog(@"shouldEndEditing");
+    return YES;
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    NSLog(@"didEndEditing");
+    [[self doneButton] setEnabled:NO];
+}
+
+- (IBAction)hideKeyboard:(id)sender {
+    [[self babyText] resignFirstResponder];
+}
+
 
 @end
